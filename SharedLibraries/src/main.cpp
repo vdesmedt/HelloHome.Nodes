a6640d4cf386a63e7f920847edd9c0b8528b6f7b
Valery De Smedt <valery@indri.be>
2021-11-28 16:10:48 +0100

    Setup basic node

--- SharedLibraries/src/main.cpp
+++ SharedLibraries/src/main.cpp
@@ -1,4 +1,28 @@
 #include <Arduino.h>
+#include "HHLogger/HHLogger.h"
+#include "HHCentral/HHCentral.h"
+#include "HHMessages/HHMessages.h"
 
-void setup() {}
-void loop() {}
+HHLogger *logger;
+HHCentral *hhCentral;
+
+void setup()
+{
+    logger = new HHLogger(LogMode::Off);
+    hhCentral = new HHCentral(logger, NodeType::Simulator, "1.0", HHEnv::Dev);
+    hhCentral->connect(true);
+}
+
+void loop()
+{
+    Command *cmd;
+    cmd = hhCentral->check();
+    if (cmd != nullptr)
+    {
+        switch (cmd->msgType)
+        {
+        case CMD_NODECONFIG:
+            break;
+        }
+    }
+}
